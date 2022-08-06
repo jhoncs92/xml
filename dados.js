921205ee7ec950de00fe41b0e45d75f8d54cd2d8
jhoncs92 <110737182+jhoncs92@users.noreply.github.com>
2022-08-06 13:54:57 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -6,33 +6,33 @@ let dado=`
         <preço>50</preço>
     </livro>
     <livro>
-        <título>Meu livro</título>
-        <autor>José Silva</autor>
-        <ano>2020</ano>
+        <título>ORCA </título>
+        <autor>Marcelo</autor>
+        <ano>2019</ano>
         <preço>50</preço>
     </livro>
     <livro>
-        <título>Meu livro</título>
-        <autor>José Silva</autor>
-        <ano>2020</ano>
+        <título>Leandro</título>
+        <autor> Silva</autor>
+        <ano>2000</ano>
         <preço>50</preço>
     </livro>
     <livro>
-        <título>Meu livro</título>
-        <autor>José Silva</autor>
-        <ano>2020</ano>
+        <título>Bela e a Fera</título>
+        <autor>A fera</autor>
+        <ano>2008</ano>
         <preço>50</preço>
     </livro>
     <livro>
-        <título>Meu livro</título>
-        <autor>José Silva</autor>
-        <ano>2020</ano>
+        <título>Cabloco</título>
+        <autor>Jauro</autor>
+        <ano>2010</ano>
         <preço>50</preço>
     </livro>
     <livro>
-        <título>Meu livro</título>
-        <autor>José Silva</autor>
-        <ano>2020</ano>
+        <título>Arroz</título>
+        <autor>Alex</autor>
+        <ano>2014</ano>
         <preço>50</preço>
     </livro>
 `;
